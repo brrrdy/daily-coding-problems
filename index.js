984a4ef1c8b5d30205cd8ca3f7e385edeb01f668
Cody <4109747+brrrdy@users.noreply.github.com>
2022-03-07 19:00:26 +0000

    Move to daily archive

--- index.js
+++ index.js
@@ -1,47 +1,10 @@
 /**
  * Author: Cody Zornes
- * Date: 2022/03/07
+ * Date: 
  * 
- * DCP #33
- * Desc: Compute the running median of a sequence of numbers. That is, 
- * given a stream of numbers, print out the median of the list so far 
- * on each new element.
+ * DCP / LC / HR #
+ * Desc:
  * 
- * Ex input: [2, 1, 5, 7, 2, 0, 5]
+ * Ex input: 
  * Ex output:
- * 2
- * 1.5
- * 2
- * 3.5
- * 2
- * 2
- * 2
  */
-
-function runningMedian(stream) {
-  let median = 0;
-  let sorted = [];
-
-  // O(N)
-  for (let i = 0; i < stream.length; i++) {
-    sorted.push(stream[i]);
-    // O(NlogN)
-    sorted = sorted.sort((a,b) => a-b);
-    //console.log(sorted);
-    if (i === 0) {
-      median = stream[i];
-    } else {
-      let n = sorted.length/2;
-      if (sorted.length % 2 === 0) { // even #
-        median = (sorted[n] + sorted[n-1])/2;
-      } else { // odd #
-        median = sorted[Math.floor(n)];
-      }
-    }
-    console.log(median);
-  }
-}
-
-const s1 = [2, 1, 5, 7, 2, 0, 5];
-
-runningMedian(s1);
